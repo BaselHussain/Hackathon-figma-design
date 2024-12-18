a5825088e405cb402f134c11a0b1096ea4a23633
BaselHussain <154091057+BaselHussain@users.noreply.github.com>
2024-12-18 21:06:05 +0500

    context-api

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Button } from '@/components/ui/button'
 import { CartContext } from '@/context'
 import React,{useContext} from 'react'
 
@@ -6,23 +7,22 @@ import React,{useContext} from 'react'
 
 export default function page() {
     const cartItems=useContext(CartContext)
-    const id:any=cartItems.cart.map((item)=>{
-        return(
-            item.id
-        )
-    })
+  
   return (
     <>
-    <div>
-        asas
+    
+      
       {cartItems.cart.map((item)=>(
-        <p>{item.title}
-        {item.description}
-        </p>
+        <div key={item.id} className='flex space-x-10'>
+          <p>{item.title}</p>
+          <p>{item.price}</p>
+<Button onClick={()=>cartItems.handleDeleteItem(item.id)}>Delete</Button>
+          
+        </div>
         
       ))}
-      {cartItems.handleDeleteItem(id)}
-    </div>
+     <div>{cartItems.total}</div>
+    
     </>
   )
 }
